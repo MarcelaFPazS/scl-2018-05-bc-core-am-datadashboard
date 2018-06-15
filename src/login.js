e567b8d243c790fc65c7fb6a082368d0e2539fe3
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-06-15 10:36:50 -0400

    estrucura login

---
+++ src/login.js
@@ -0,0 +1,12 @@
+function entrar() {
+  if (document.form.email.value === 'valentina@laboratoria.cl' && document.form.password.value === 'laboratoria') {
+    const btnhide = document.getElementById('buttonlogin');
+    btnhide.addEventListener('click', () => {
+      document.getElementsByClassName('ingresar')[0].classList.add('buttonlogin');
+      document.getElementsByClassName('afterEntering')[0].classList.remove('afterEntering');
+    }
+    );
+  } else {
+    document.getElementById('errorPassword').innerHTML = '¡Datos inválidos! Vuelve a intentarlo.';
+  }
+}
