0147ecca20279f88ae349feb3fd990e90ea084e1
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-06-18 20:22:08 -0400

    modificando login

--- src/login.js
+++ src/login.js
@@ -1,12 +1,14 @@
+const btn = document.getElementById('btnlog');
+
+
 function entrar() {
-  if (document.form.email.value === 'valentina@laboratoria.cl' && document.form.password.value === 'laboratoria') {
-    const btnhide = document.getElementById('buttonlogin');
-    btnhide.addEventListener('click', () => {
-      document.getElementsByClassName('ingresar')[0].classList.add('buttonlogin');
+  if (document.form.email.value === 'valentina@laboratoria.cl' && document.form.contraseña.value === 'laboratoria') {
+    btn.addEventListener('click', () => {
+      document.getElementById('inlineFormInputGroupUsername2')[0].classList.add('btnlog');
       document.getElementsByClassName('afterEntering')[0].classList.remove('afterEntering');
     }
     );
   } else {
-    document.getElementById('errorPassword').innerHTML = '¡Datos inválidos! Vuelve a intentarlo.';
+    document.getElementById('errorPassword').innerHTML = 'Datos invalidos, favor verificar correo electrónico y contraseña';
   }
 }
