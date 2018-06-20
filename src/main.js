0440514668e4b4ee56e89ba860ead1ec2973fd6a
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-06-20 11:49:30 -0400

    enlace main con estgeneral

--- src/main.js
+++ src/main.js
@@ -18,8 +18,16 @@ const renderUsers = data => {
       const contenedorData = document.createElement('div');
       const contenedor = document.getElementById('contenedor');
       contenedor.appendChild(contenedorData);
+      //crear un a que sea padre de titulo text
+      const lim = document.createElement('a');
+      contenedorData.appendChild(lim);
+      //agregarle atributo href
+      lim.setAttribute('href', 'estgeneral.html');
+      //indicarle a donde quieren enlazar
+      
+
       let titulo_text= document.createTextNode(element.id);
-      contenedorData.appendChild(titulo_text);
+      lim.appendChild(titulo_text);
     })
     return contenedorData;
   })
