51e416dfbe763ecc6680d4026e28a96ef6b6fb95
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-06-22 08:30:08 -0400

    agregando lo borrado en comentario

--- src/main.js
+++ src/main.js
@@ -54,10 +54,49 @@ function computeUsersStats () {
   }
 }
 
+/*
+function loadAllData () {
+// Concadena las url para enviar a la api 
+  const url1 = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+  const url2 = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
+  const url3 = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+  const btnlista = document.getElementById('btnlista'); 
+  const container = document.getElementById('root');
+
+  Promise.all([   //Ejecuta todas las llamadas de manera paralela
+    fetch(url1),
+    fetch(url2),
+    fetch(url3)
+    ]).then(
+      (responses)=>{   //Responde a todas las promesas
+        return Promise.all(responses.map((response)=>{
+          return response.json();
+        }));
+      }
+    ).then((responseJsons)=>{ //Arreglo de respuestas en json
+         responseJsons^*
+         // 
+         //
+         console.log("holi");
+    }).catch(
+        (error)=>{ 
+        }
+    );
+  
+    btnlista.addEventListener('click', () => {
+      const render = data.forEach(element => {
+        //element.name === arreglo[i].name
+        return container.innerHTML += `<li>${element.name}  ${element.percent} ${element}</li>`;
+      });
+    });
+
+}
+*/
+
 function searchStudent() {
   //input vacio desde el que lo saco
   const buscar = document.getElementById('buscar').value;
-  const filtro = window.filterUsers(usersData, search);
+  const filtro = window.filterUsers(users, search);
   const table = document.getElementById('search-a');
  
   renderUserTable(usersFiltered);
