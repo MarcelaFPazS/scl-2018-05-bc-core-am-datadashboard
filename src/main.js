e91b8d16e874662eba0fff0b0e8aa61abd6060a7
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-07-24 10:07:19 -0400

    tomando lo de fabian

--- src/main.js
+++ src/main.js
@@ -1,24 +1,61 @@
-//variables globales
-let users = [];
+let users = null;
+let progress = null;
+let cohorts = null; 
+let usersStats = null;
+
+//funciones
+window.onload = () => {
+  dataJSON();
+  areWeFinishedYet()
+
+}
+
+fetch('https://fabianbravoa.github.io/2018-1-SolucionarioDatadashboard/data/cohorts/lim-2018-03-pre-core-pw/users.json')
+  .then(response => response.json())
+  .then(usersJSON => {
+    users = usersJSON;
+    areWeFinishedYet();
+  })
+  .catch(error => {
+    console.error("No pudimos obtener usuarios");
+    //console.error indica un mensaje de error, indica una alerta grave
+    console.error("ERROR > " + error.stack); //error.stack muestra donde falló el codigo, imprime donde esta el error
+  });
+
+  fetch('https://fabianbravoa.github.io/2018-1-SolucionarioDatadashboard/data/cohorts/lim-2018-03-pre-core-pw/progress.json')
+  .then(response => response.json())
+  .then(progressJSON => {
+    progress = progressJSON;
+    areWeFinishedYet();
+  })
+  .catch(error => {
+    console.error("No pudimos obtener el progreso");
+    console.error("ERROR > " + error.stack);
+  });
+
+  fetch('https://fabianbravoa.github.io/2018-1-SolucionarioDatadashboard/data/cohorts.json')
+  .then(response => response.json())
+  .then(cohortsJSON => {
+    cohorts = cohortsJSON;
+    areWeFinishedYet();//se llama en todas las respuestas por que no sabemos cual llegara primero y asi nos aseguramos si se ejecutan
+  })
+  .catch(error => {
+    console.error("No pudimos obtener el listado de cohorts");
+    console.error("ERROR > " + error.stack);
+  });
+
 
 //Para login con alert
 let go = () => { //solo funciona con este nombre y contraseña
-if (document.form.password.value ==='laboratoria' &&  document.form.login.value ==='Valentina'){ 
+  if (document.form.password.value ==='laboratoria' &&  document.form.login.value ==='Valentina'){ 
   window.open('menu.html') //abre página index si se pone correcto
   } 
   else{ 
     alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
   } 
-} 
-
-//funciones
-window.onload = () => {
-  dataJSON();
-  computeUsersStats();
-  searchStudent();
 }
 
-// data cohorts
+  // data cohorts
 function dataJSON() {
   const btnCohort = document.getElementById('btncont');
   const usersJSON = '../data/cohorts.json';
@@ -42,6 +79,51 @@ function dataJSON() {
     })
   }
 }
+
+
+function areWeFinishedYet() { //¿hemos terminado?
+  // se llama desde todas las promesas para que tome los tome en cuenta sin importar cual de ellos se ejecute primero
+  //vemos si users progress y cohorts ya tienen datos en su interior sino no se ejecuta
+  if (users && progress && cohorts) {
+    const cohort = cohorts.find(item => item.id === 'lim-2018-03-pre-core-pw'); //busca el cohort que tiene ese id ya que este es el unico cohort que esta en los json
+    const courses = Object.keys(cohort.coursesIndex);
+    // guardamos el resultado de llamar a la funcion en una variable global    
+    usersStats = window.computeUsersStats(users, progress, courses);//recibe users, progress y el listado de los cursos del cohort
+  }
+}
+//se declara la funcion para darle funcionalidad al boton toggler y que cambie al darle click
+function onToggleSort() {
+  const direction = toggleSort.innerText;
+  if (direction == "ASC") {
+    toggleSort.innerText = "DESC";
+  } else {
+    toggleSort.innerText = "ASC";
+  }
+  //llamamos a la funcion de ordenamiento para que que ordene los usuarios
+  const sortedUsers = window.sortUsers(usersStats, "percent", direction);
+  //no se hace el getElementById por que en JS todo lo declarado en el html con un id queda como variable global :O
+  studentContainer.innerHTML = "";
+  for(let student of sortedUsers){
+    studentContainer.innerHTML += `
+      <p>${student.name} ${student.stats.percent}</p>
+    `;
+  }
+}
+
+function onSearchBoxChange(){
+  const search = searchBox.value;
+  const filteredUsers = window.filterUsers(usersStats, search);
+  studentContainer.innerHTML = "";
+  filteredUsers.forEach(student => {
+    studentContainer.innerHTML += `
+      <p>${student.name}</p>
+    `;
+  });
+}
+
+
+/*
+
 
 //data lim-2018-03-pre-core-pw
 function computeUsersStats() {
@@ -70,7 +152,7 @@ function computeUsersStats() {
     })
   }
 }
-
+*/
 /*
 function loadAllData () {
 // Concadena las url para enviar a la api 
@@ -109,7 +191,7 @@ function loadAllData () {
 
 }
 */
-
+/*
 //filtrar lista alumnas
 function searchStudent() {
   //input vacio desde el que lo saco
@@ -134,4 +216,4 @@ function searchStudent() {
       }
   }
 }
-
+*/
