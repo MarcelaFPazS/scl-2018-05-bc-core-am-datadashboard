b62d325edbe9d4ca43b634c5607e70f6ff5f150d
Marcela Paz <marcela.fpaz.s@gmail.com>
2018-07-24 10:09:19 -0400

    cambios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 //variables globales
-let users = [];
+/*let users = [];
 
 //Para login con alert
 let go = () => { //solo funciona con este nombre y contraseña
@@ -66,16 +66,10 @@ function computeUsersStats() {
       let percent = null;
       return containerName.innerHTML += `<p>${element.name}</p>`
       })
-<<<<<<< HEAD
-      return render;
-      console.log(render);
-      
-=======
       return containerRanking.innerHTML += ranking
->>>>>>> upstream/master
     })
   }
-}
+}*/
 
 /*
 function loadAllData () {
@@ -117,7 +111,7 @@ function loadAllData () {
 */
 
 //filtrar lista alumnas
-function searchStudent() {
+/*function searchStudent() {
   //input vacio desde el que lo saco
   const buscar = document.getElementById('buscar');
   //enlace con data.js
@@ -139,5 +133,87 @@ function searchStudent() {
       return "busca nuevamente"
       }
   }
+}*/
+
+
+let users = null;
+let progress = null;
+let cohorts = null; //null == false => true
+// null para que sea false siempre, llaves no, corchetes si.
+let usersStats = null;
+
+// se utiliza el link de git para no usar el server 
+//el link es hacia una api de ususarios que esta online
+fetch('https://github.com/MarcelaFPazS/scl-2018-05-bc-core-am-datadashboard/blob/master/data/cohorts/lim-2018-03-pre-core-pw/progress.json')
+  .then(response => response.json())
+  .then(usersJSON => {
+    users = usersJSON;
+    areWeFinishedYet();
+  })
+  .catch(error => {
+    console.error("No pudimos obtener usuarios");
+    //console.error indica un mensaje de error, indica una alerta grave
+    console.error("ERROR > " + error.stack); //error.stack muestra donde falló el codigo, imprime donde esta el error
+  });
+// se utiliza url relativa de gh pages.
+fetch('https://github.com/MarcelaFPazS/scl-2018-05-bc-core-am-datadashboard/blob/master/data/cohorts/lim-2018-03-pre-core-pw/progress.json')
+  .then(response => response.json())
+  .then(progressJSON => {
+    progress = progressJSON;
+    areWeFinishedYet();
+  })
+  .catch(error => {
+    console.error("No pudimos obtener el progreso");
+    console.error("ERROR > " + error.stack);
+  });
+
+fetch('https://github.com/MarcelaFPazS/scl-2018-05-bc-core-am-datadashboard/blob/master/data/cohorts/lim-2018-03-pre-core-pw/progress.json')
+  .then(response => response.json())
+  .then(cohortsJSON => {
+    cohorts = cohortsJSON;
+    areWeFinishedYet();//se llama en todas las respuestas por que no sabemos cual llegara primero y asi nos aseguramos si se ejecutan
+  })
+  .catch(error => {
+    console.error("No pudimos obtener el listado de cohorts");
+    console.error("ERROR > " + error.stack);
+  });
+
+function areWeFinishedYet() { //¿hemos terminado?
+  // se llama desde todas las promesas para que tome los tome en cuenta sin importar cual de ellos se ejecute primero
+  //vemos si users progress y cohorts ya tienen datos en su interior sino no se ejecuta
+  if (users && progress && cohorts) {
+    const cohort = cohorts.find(item => item.id === 'lim-2018-03-pre-core-pw'); //busca el cohort que tiene ese id ya que este es el unico cohort que esta en los json
+    const courses = Object.keys(cohort.coursesIndex);
+    // guardamos el resultado de llamar a la funcion en una variable global    
+    usersStats = window.computeUsersStats(users, progress, courses);//recibe users, progress y el listado de los cursos del cohort
+  }
+}
+//se declara la funcion para darle funcionalidad al boton toggler y que cambie al darle click
+function onToggleSort() {
+  const direction = toggleSort.innerText;
+  if (direction == "ASC") {
+    toggleSort.innerText = "DESC";
+  } else {
+    toggleSort.innerText = "ASC";
+  }
+  //llamamos a la funcion de ordenamiento para que que ordene los usuarios
+  const sortedUsers = window.sortUsers(usersStats, "percent", direction);
+  //no se hace el getElementById por que en JS todo lo declarado en el html con un id queda como variable global :O
+  studentContainer.innerHTML = "";
+  for(let student of sortedUsers){
+    studentContainer.innerHTML += `
+      <p>${student.name} ${student.stats.percent}</p>
+    `;
+  }
 }
 
+function onSearchBoxChange(){
+  const search = searchBox.value;
+  const filteredUsers = window.filterUsers(usersStats, search);
+  studentContainer.innerHTML = "";
+  filteredUsers.forEach(student => {
+    studentContainer.innerHTML += `
+      <p>${student.name}</p>
+    `;
+  });
+}
